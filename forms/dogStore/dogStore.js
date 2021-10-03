f2095bd0f1ba0f1eee2b02b435a3747f53739fa3
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-10-03 18:28:02 -0500

    update

--- forms/dogStore/dogStore.js
+++ forms/dogStore/dogStore.js
@@ -1,4 +1,6 @@
-let products = ["Doy treats $12.00", "Dog leash $ 15.00", "Dog medicine $200.00"]
+
+let products = ["Crocheted Dog Toy, $10.00", "Slow Feeder Bowl, $14.00", "Pawz Grey Treat Jar, $20.00", "Blue Sweet Dreams Thermal Pajamas, $30.00",
+"Checker Chewy Vuiton Bowl, $32.00", "Red Heart Sweater Dress, $27.00", "Velvet Hair Bow - Sun Kissed, $12.00", "Into the Wild Leash, $27.00"]
 
 btnProducts.onclick=function(){
   let display = lblItem.value
