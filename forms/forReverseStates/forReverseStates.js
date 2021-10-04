79eb1cb8f5756e6134aa0c0fb43c924791c693d8
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-10-03 22:09:30 -0500

    change

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,4 +1,4 @@
-let states = ["NE", "NY", "TX", "KS", "WA"]
+let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas","Oklahoma", "Texas"]
 
 for ( i = states.length - 1; i >= 0; i--)
   console.log(states[i])
